3f18125264dfd5e8b2ed719549cf2e644646cb1c
didapax <alfonsi.acosta@gmail.com>
2024-10-04 17:55:11 -0400

    Update servermail.php

--- servermail.php
+++ servermail.php
@@ -16,8 +16,8 @@ function sendEmail($to, $subject, $body) {
     $mail->isSMTP();
     $mail->Host = 'server121.web-hosting.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'criptosignalgroup@criptosignalgroup.online';
-    $mail->Password = 'JRnc^YaDj@la';
+    $mail->Username = '';
+    $mail->Password = '';
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
     $mail->Port = 587;
 
